49af546af153c1b71ab47eb7f7009200f6c1d724
hoangnhfpl <mume.hoangnh@gmail.com>
2022-01-14 17:53:41 +0700

    auto increment example

--- resources/views/livewire/counter.blade.php
+++ resources/views/livewire/counter.blade.php
@@ -1,3 +1,7 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
+<div style="text-align: center">
+    <h1>Hello, Laravel-livewire</h1>
+{{--    <button wire:click="increment">+</button>--}}
+    <h1>{{ $count }}</h1>
+
+    <p wire:poll.1000ms="increment"></p>
 </div>
